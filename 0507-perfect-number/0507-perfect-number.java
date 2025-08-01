1afc5324213016ca97dcfb29dbd11bfe946867eb
Sukisivam Balakrishnan <122740112+sukisivam29@users.noreply.github.com>
2025-08-01 13:53:47 +0530

    Time: 2 ms (80.24%), Space: 40.5 MB (41.27%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+         if (num <= 1) return false;
+        int sum = 1;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                sum += i;
+                int pair = num / i;
+                if (pair != i) sum += pair;
+            }
+        }
+        return sum == num;
+    }
+}
